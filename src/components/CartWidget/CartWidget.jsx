49237229694fe95e37fc2c7d706fcd7fb30e8b9f
fernandorusso <russofg@gmail.com>
2021-11-01 12:16:50 -0300

    Se corrigieron las rutas de las imagenes

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ import './CartWidget.css';
 
 const CartWidget = () => {
     return (
-        <button className="navbar-cart"><Link to="/cart"><img src="assets/AddCart.png"  width="40"
+        <button className="navbar-cart"><Link to="/cart"><img src="../../assets/AddCart.png"  width="40"
         height="40"
         className="d-inline-block align-top"alt="Carrito"/></Link>
             
--- src/services/GetFetchList.jsx
+++ src/services/GetFetchList.jsx
@@ -1,15 +1,15 @@
 const charlas = [
-    { id:"01", categoria:"Programación", title:"React", speaker:"Juan", price:5000, pictureUrl:"assets/react.png", stock:10},
-    { id:"02", categoria:"Programación", title:"Java", speaker:"Pedro", price:5000, pictureUrl:"assets/java.png", stock:20},
-    { id:"03", categoria:"Programación", title:"Vue", speaker:"Juan", price:5000, pictureUrl:"assets/vue.png", stock:10},
-    { id:"04", categoria:"Programación", title:"JavaScript", speaker:"Pedro", price:14000, pictureUrl:"assets/js.png", stock:30},
-    { id:"05", categoria:"Programación", title:"Html", speaker:"Juan", price:3000, pictureUrl:"assets/html.png", stock:10},
-    { id:"06", categoria:"Programación", title:"CSS", speaker:"Pedro", price:4000, pictureUrl:"assets/css.png", stock:5},
-    { id:"07", categoria:"Edición", title:"Adobe Premiere", speaker:"Juan", price:10000, pictureUrl:"assets/premiere.png", stock:15},
-    { id:"08", categoria:"Edición", title:"Adobe After Effects", speaker:"Pedro", price:12000, pictureUrl:"assets/after-effects.png", stock:20},
-    { id:"09", categoria:"Edición", title:"Adobe Audition", speaker:"Juan", price:8000, pictureUrl:"assets/audition.png", stock:10},
-    { id:"10", categoria:"Edición", title:"Adobe Photoshop ", speaker:"Pedro", price:5000, pictureUrl:"assets/adobe-photoshop.png", stock:20},
-    { id:"11", categoria:"Edición", title:"Adobe Illustrador", speaker:"Juan", price:5000, pictureUrl:"assets/adobe-illustrator.png", stock:10},
+    { id:"01", categoria:"Programación", title:"React", speaker:"Juan", price:5000, pictureUrl:"../../assets/react.png", stock:10},
+    { id:"02", categoria:"Programación", title:"Java", speaker:"Pedro", price:5000, pictureUrl:"../../assets/java.png", stock:20},
+    { id:"03", categoria:"Programación", title:"Vue", speaker:"Juan", price:5000, pictureUrl:"../../assets/vue.png", stock:10},
+    { id:"04", categoria:"Programación", title:"JavaScript", speaker:"Pedro", price:14000, pictureUrl:"../../assets/js.png", stock:30},
+    { id:"05", categoria:"Programación", title:"Html", speaker:"Juan", price:3000, pictureUrl:"../../assets/html.png", stock:10},
+    { id:"06", categoria:"Programación", title:"CSS", speaker:"Pedro", price:4000, pictureUrl:"../../assets/css.png", stock:5},
+    { id:"07", categoria:"Edición", title:"Adobe Premiere", speaker:"Juan", price:10000, pictureUrl:"../../assets/premiere.png", stock:15},
+    { id:"08", categoria:"Edición", title:"Adobe After Effects", speaker:"Pedro", price:12000, pictureUrl:"../../assets/after-effects.png", stock:20},
+    { id:"09", categoria:"Edición", title:"Adobe Audition", speaker:"Juan", price:8000, pictureUrl:"../../assets/audition.png", stock:10},
+    { id:"10", categoria:"Edición", title:"Adobe Photoshop ", speaker:"Pedro", price:5000, pictureUrl:"../../assets/adobe-photoshop.png", stock:20},
+    { id:"11", categoria:"Edición", title:"Adobe Illustrador", speaker:"Juan", price:5000, pictureUrl:"../../assets/adobe-illustrator.png", stock:10},
 ];
 
 const GetFetchList = new Promise ((resolve, reject) => {
